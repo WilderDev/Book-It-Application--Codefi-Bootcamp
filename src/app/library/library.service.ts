b6e70c58e0261e58a592318e097be48a73c51576
WilderDev <williamjohnwilder@outlook.com>
2022-05-09 08:15:32 -0500

    FIX: http to https

--- src/app/library/library.service.ts
+++ src/app/library/library.service.ts
@@ -1,10 +1,12 @@
-import { Injectable } from '@angular/core';
-import { Book } from '../shared/book/book.model';
-import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Book } from '../shared/book/book.model';
+
 @Injectable({
-  providedIn: 'root', // Provide in the root of our application
+  providedIn: "root" // Provide in the root of our application
 })
 export class LibraryService {
   apiBooksChanged = new Subject<Book[]>();
@@ -20,11 +22,14 @@ export class LibraryService {
 
   onFetchBooks(query: string) {
     // Turn Search Query into lowercase words with plus sign for spaces
-    const formattedQuery = query.split(' ').join('+').toLowerCase();
+    const formattedQuery = query
+      .split(" ")
+      .join("+")
+      .toLowerCase();
 
     // Send HTTP GET Request to the "openLibrary" api endpoint using the tranformed input query
     this.http
-      .get(`http://openlibrary.org/search.json?q=${formattedQuery}`)
+      .get(`https://openlibrary.org/search.json?q=${formattedQuery}`)
       .subscribe((searchResults: any) => {
         const bestMatches = searchResults.docs.slice(0, 7);
 
@@ -33,12 +38,12 @@ export class LibraryService {
   }
 
   saveBooksToGlobalArray(books) {
-    books.map((book) => {
+    books.map(book => {
       const formattedBook = new Book(
         book.title,
-        book.author_name ? book.author_name[0] : 'unknown',
-        'unknown',
-        'https://tse2.mm.bing.net/th?id=OIP.I6LGwie40Vw4K8gmV52MKwHaLc&pid=Api&P=0&w=300&h=300'
+        book.author_name ? book.author_name[0] : "unknown",
+        "unknown",
+        "https://tse2.mm.bing.net/th?id=OIP.I6LGwie40Vw4K8gmV52MKwHaLc&pid=Api&P=0&w=300&h=300"
       );
       this.allLibraryResults.push(formattedBook);
     });
