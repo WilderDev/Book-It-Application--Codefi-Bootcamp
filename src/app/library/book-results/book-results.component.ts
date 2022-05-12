20bebc8288e08710da9e4996a44cc9c9997b986e
WilderDev <williamjohnwilder@outlook.com>
2022-05-12 12:14:20 -0500

    ADD: Animations + PWA

--- src/app/library/book-results/book-results.component.ts
+++ src/app/library/book-results/book-results.component.ts
@@ -1,12 +1,62 @@
+import { animate, keyframes, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+
+import { BookshelfService } from '../../bookshelf/bookshelf.service';
 import { Book } from '../../shared/book/book.model';
 import { LibraryService } from '../library.service';
-import { BookshelfService } from '../../bookshelf/bookshelf.service';
 
 @Component({
-  selector: 'app-book-results',
-  templateUrl: './book-results.component.html',
-  styleUrls: ['./book-results.component.css'],
+  selector: "app-book-results",
+  templateUrl: "./book-results.component.html",
+  styleUrls: ["./book-results.component.css"],
+  animations: [
+    trigger("swoopIn", [
+      state(
+        "in",
+        style({
+          transform: "scale(1)",
+          opacity: 1
+        })
+      ),
+      transition(":enter", [
+        animate(
+          "1s",
+          keyframes([
+            style({
+              transform: "scale(0)",
+              opacity: 0,
+              offset: 0
+            }),
+            style({
+              transform: "scale(0.2)",
+              opacity: 0.2,
+              offset: 0.2
+            }),
+            style({
+              transform: "scale(0.4)",
+              opacity: 0.4,
+              offset: 0.4
+            }),
+            style({
+              transform: "scale(0.6)",
+              opacity: 0.6,
+              offset: 0.6
+            }),
+            style({
+              transform: "scale(0.8)",
+              opacity: 0.8,
+              offset: 0.8
+            }),
+            style({
+              transform: "scale(1)",
+              opacity: 1,
+              offset: 1
+            })
+          ])
+        )
+      ])
+    ])
+  ]
 })
 export class BookResultsComponent implements OnInit {
   libraryBooks: Book[] = [];
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { BookComponent } from './book/book.component';
